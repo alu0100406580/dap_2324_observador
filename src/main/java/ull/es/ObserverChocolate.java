2630c789e00a2ba4b6659966620a0fea4da1b650
Dario Afonso <alu0100406580@ull.edu.es>
2023-12-01 02:07:16 +0000

    Creando la clase ObserverChocolate

---
+++ src/main/java/ull/es/ObserverChocolate.java
@@ -0,0 +1,50 @@
+package ull.es;
+
+import java.util.List;
+import java.util.Objects;
+
+public class ObserverChocolate implements Observer {
+    String name;
+    List<Product> productsList;
+    List<Product> productListNew;
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+
+        ObserverChocolate otherObserver = (ObserverChocolate) obj;
+
+        return Objects.equals(this.name, otherObserver.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+
+    ObserverChocolate(String name) {
+        this.name = name;
+    }
+
+    String getName() {
+        return this.name;
+    }
+    @Override
+    public void update(List<Product> products, List<Product> newProductList) {
+        this.productsList = products;
+        this.productListNew = newProductList;
+    }
+
+    public List<Product> getProductsList() {
+        return productsList;
+    }
+
+    public List<Product> getProductListNew() {
+        return productListNew;
+    }
+}
